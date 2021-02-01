0192668ec76f092b1575d2a24ec62f8f7dc542cf
LuanaDG <luanadelagiustina@gmail.com>
2021-02-01 16:42:28 -0300

    cadastro de usuários e rotas

--- src/main/rotas.js
+++ src/main/rotas.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Route, Switch, HashRouter, Router } from 'react-router-dom';
 import Login from '../views/login'
+import CadastroUsuario from '../views/cadastroUsuario'
 
 function Rotas(){
   return(
     <HashRouter>
       <Switch>
         <Route path="/login" component={Login} />
+        <Route path="/cadastro-usuarios" component={CadastroUsuario} />
       </Switch>
     </HashRouter>
+
   )
 }
