cc449bbb2c02949c35ee2fa9c0fc6ea741528151
LuanaDG <luanadelagiustina@gmail.com>
2021-02-03 10:31:41 -0300

    cadastro de usuários

--- src/views/home.js
+++ src/views/home.js
@@ -1,13 +1,33 @@
 
 import React from 'react';
+import UsuarioService from '../app/service/usuarioService'
+import LocalStorageService from '../app/service/localstorageService'
 
-class Home extends React.Component{
+
+class Home extends React.Component {
 
   state = {
     saldo: 0
   }
 
-  render(){
+  constructor() {
+    super()
+    this.UsuarioService = new UsuarioService();
+  }
+
+  componentDidMount() {
+    const usuarioLogado = LocalStorageService.obterItem('_usuario_logado')
+
+    this.UsuarioService
+      .obterSaldoPorUsuario(usuarioLogado.id)
+      .then(response => {
+        this.setState({ saldo: response.data })
+      }).catch(error => {
+        console.error(error.response)
+      })
+  }
+
+  render() {
     return (
       <div className="jumbotron">
         <h1 className="display-3">Bem vindo!</h1>
@@ -17,13 +37,13 @@ class Home extends React.Component{
         <p>Essa é a sua área administrativa, utilize um dos menus ou botões abaixo para navegar pelo sistema.</p>
         <p className="lead">
           <a className="btn btn-primary btn-lg"
-          href="#/cadastro-usuarios"
-          role="button"><i className="fa fa-users"></i>
+            href="#/cadastro-usuarios"
+            role="button"><i className="fa fa-users"></i>
           Cadastrar Usuário
           </a>
           <a className="btn btn-danger btn-lg"
-          href=""
-          role="button"><i className="fa fa-users"></i>
+            href=""
+            role="button"><i className="fa fa-users"></i>
           Cadastrar Lançamento
           </a>
         </p>
