fabc9fe2f798ce2a9310b0d66a598704f0d4cf42
LuanaDG <luanadelagiustina@gmail.com>
2021-02-03 21:25:19 -0300

    tela Consulta Lancamentos

--- src/main/rotas.js
+++ src/main/rotas.js
@@ -4,6 +4,7 @@ import { Route, Switch, HashRouter, Router } from 'react-router-dom';
 import Login from '../views/login'
 import CadastroUsuario from '../views/cadastroUsuario'
 import Home from '../views/home'
+import ConsultaLancamentos from '../views/lancamentos/consulta-lancamentos'
 
 function Rotas(){
   return(
@@ -12,6 +13,7 @@ function Rotas(){
       <Route path="/home" component={Home} />
         <Route path="/login" component={Login} />
         <Route path="/cadastro-usuarios" component={CadastroUsuario} />
+        <Route path="/consulta-lancamentos" component={ConsultaLancamentos} />
       </Switch>
     </HashRouter>
 
